187b8814e31c621a95846248d3b4583d8de68573
NekuSakuraba <drimiksi@gmail.com>
2019-09-25 13:16:38 -0400

    Tested Restaurant JPA Mapping

--- src/main/java/com/example/bootiful_restaurant_reviews/model/Restaurant.java
+++ src/main/java/com/example/bootiful_restaurant_reviews/model/Restaurant.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
 }
--- src/test/java/com/example/bootiful_restaurant_reviews/model/RestaurantJpaTest.java
+++ src/test/java/com/example/bootiful_restaurant_reviews/model/RestaurantJpaTest.java
@@ -1,4 +1,34 @@
 package com.example.bootiful_restaurant_reviews.model;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
 public class RestaurantJpaTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void testRestaurantMapping() {
+        Restaurant restaurant = testEntityManager.persistFlushFind(new Restaurant(null, "John Beef"));
+
+        assertThat(restaurant.getId()).isGreaterThan(0L);
+        assertThat(restaurant.getName()).isEqualTo("John Beef");
+
+        Long id = restaurant.getId();
+
+        restaurant.setName("John Super");
+        restaurant = testEntityManager.persistFlushFind(restaurant);
+
+        assertThat(restaurant.getId()).isEqualTo(id);
+        assertThat(restaurant.getName()).isEqualTo("John Super");
+    }
 }
